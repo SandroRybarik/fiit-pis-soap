abb8acfb6e2267333db5c75b1911d8a428c683d1
Sandro Rybarik <sandro.rybarik@gmail.com>
2020-05-05 23:54:04 +0200

    Customer create + email validation service + cities service
    Co-authored-by: Adam Štuller <adam.syn007@gmail.com>
    Co-authored-by: simonsadecky@gmail.com <simonsadecky@gmail.com>

--- routes/customer.js
+++ routes/customer.js
@@ -1,9 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const { soapRequest, wsdl } = require('../lib/soap')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("HELLO!");
+router.get('/create', (req, res, next) => {
+  res.render('pages/customer/create')
 });
+
+router.get('/login', (_, res) => {
+  res.render('pages/customer/login')
+})
+
+router.post('/login', (req, res) => {})
+
+router.post('/', (req, res) => {
+  const {
+    name,
+    surname,
+    email,
+    gender,
+    currency,
+    city,
+    born_at
+  } = req.body;
+
+  const validateEmail = () =>
+    soapRequest(
+      wsdl.validator, 
+      'validateEmail', 
+      { email }
+    )
+  
+  const resolveCityByName = ( name ) => 
+      soapRequest(
+        wsdl.cities_sk,
+        'getByName',
+        {
+          name
+        }
+      )
+
+  const register = ({ coord_lon, coord_lat }) =>
+    soapRequest(
+      wsdl.customer, 
+      'register', 
+      {
+        name,
+        surname,
+        email,
+        gender,
+        currency,
+        coord_lon,
+        coord_lat,
+        born_at    
+      }
+    )
+  
+  validateEmail()
+  .then( result => {
+    if(result.success)
+    return resolveCityByName(city)
+  })
+  .then(({ city }) => {
+    return register(city)
+  })
+  .then(result => {
+    res.json(result)
+  })
+  .catch( err => {
+    console.log(err)
+  })
+})
+
+
 
 module.exports = router;
