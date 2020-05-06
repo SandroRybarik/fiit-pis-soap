ec98ded470912ee7b40f9d43215d34cbc9e1eda3
Adam Stuller <adam.stuller@protonmail.com>
2020-05-06 13:01:29 +0200

    activities all

--- routes/activity.js
+++ routes/activity.js
@@ -1,32 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { soapRequest, wsdl } = require('../lib/soap')
+const { soapRequest, wsdl } = require("../lib/soap");
 
 /* GET home page. */
-router.get('/create', function(req, res, next) {
-  res.render('pages/activity/create', { title: 'Express' })
-})
+router.get("/create", function (req, res, next) {
+  res.render("pages/activity/create", { title: "Express" });
+});
+
+router.get("/", function (req, res, next) {
+  const getAllActivities = () => soapRequest(wsdl.activity, "getAll", {});
+
+  getAllActivities()
+    .then(({ activitys }) => {
+      console.log(activitys);
+      res.render("pages/activity/show_all", {
+        activities: activitys != null ? activitys.activity : [],
+      });
+    })
+    .catch(console.log);
+});
 
 // CREATE
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
+  const {
+    name,
+    start_date,
+    end_date,
+    activity_type_id,
+    trainer_id,
+    room_id,
+    branch_id,
+    confirmed,
+  } = req.body;
 
-  soapRequest(wsdl.activity, {
-    activity: {
-      name: null,
-      start_date: null,
-      end_date: null,
-      activity_type_id: req.body.activity_type_id,
-      trainer_id: req.body.trainer_id,
-      room_id: req.body.room_id,
-      branch_id: req.body.branch_id,
-      confirmed: false
-    }
-  }).then(result => {
-    res.json({ status: 'OK!' })
-  }).catch(err => {
-    console.log(err)
-    res.json({ status: 'FAIL!' })
-  })
-})
+
+  const createActivity = () => 
+    soapRequest(
+      wsdl.activity,
+      'insert',
+      {
+        activity: {
+          name: 'activitka',
+          start_date,
+          end_date,
+          activity_type_id,
+          trainer_id,
+          room_id,
+          branch_id,
+          confirmed
+        }
+      }
+    )
+
+  createActivity()
+    .then(() => {
+      res.redirect('/activity');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: "FAIL!" });
+    });
+});
 
 module.exports = router;
