64676cce417fd1b0c318a636b92c1a6d609adfd0
Adam Stuller <adam.stuller@protonmail.com>
2020-05-15 16:41:45 +0200

    activity type refactored

--- src/controllers/activityType.js
+++ src/controllers/activityType.js
@@ -1,42 +1,27 @@
-const { soapRequest, wsdl } = require('../lib/soap')
-
+const { soapRequest, wsdl } = require("../lib/soap");
 
 const createActivityType = async (req, res) => {
-
-    const {
-      name,
-      price,
-      capacity,
-      description,
-    } = req.body;
-  
-    try {
-        await soapRequest(
-            wsdl.activity_type,
-            'insert',
-            {
-                activity_type: {
-                name,
-                price: Number(price),
-                capacity: Number(capacity),
-                description,
-                }
-            }
-        )
-        res.redirect('/');
-    }
-    catch(error){
-        console.log(err);
-        res.json({ status: "FAIL!" });
-    }
-    
-}
+  try {
+    await soapRequest(wsdl.activity_type, "insert", {
+      activity_type: {
+        name: req.body.name,
+        price: +req.body.price,
+        capacity: +req.body.capacity,
+        description: req.body.description,
+      },
+    });
+    res.redirect("/");
+  } catch (error) {
+    console.log(err);
+    res.json({ status: "FAIL!" });
+  }
+};
 
 const showCreateActivityType = (_, res) => {
-    res.render('pages/activityType/create');
-}
+  res.render("pages/activityType/create");
+};
 
 module.exports = {
-    showCreateActivityType,
-    createActivityType
-}
+  showCreateActivityType,
+  createActivityType,
+};
